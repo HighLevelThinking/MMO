f5e4ca47ed6bbacddb8919da5e567533e5d9b36f
lpow100 <lgn.kocher@gmail.com>
2025-01-26 12:06:47 -0800

    player move

---
+++ Assets/scripts/player_move.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    [SerializeField] int speed;
+
+    Rigidbody2D body;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.linearVelocityX = Input.GetAxis("Horizontal") * speed;
+        body.linearVelocityY = Input.GetAxis("Vertical") * speed;
+    }
+}
