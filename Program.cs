d329fb3615f598502159a12d6d33d50ad916a723
lpow100 <lgn.kocher@gmail.com>
2025-01-28 10:50:46 -0800

    um fire code

--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 TcpListener server = null;
-TcpClient client = null;
-NetworkStream stream = null;
+TcpClient[] clients;
+Dictionary<TcpClient, int> playerIdByClient;
+Dictionary<int, int[]> playerPosById;
+Dictionary<TcpClient, NetworkStream> clientStream;
 Thread thread;
 
 thread = new Thread(new ThreadStart(SetupServer));
@@ -35,28 +37,29 @@ void SetupServer()
         while (true)
         {
             Console.WriteLine("Waiting for connection...");
-            client = server.AcceptTcpClient();
+            TcpClient tempClient = server.AcceptTcpClient();
             Console.WriteLine("Connected!");
 
             message = null;
-            stream = client.GetStream();
+            clientStream[tempClient] = tempClient.GetStream();
 
             int i;
 
-            while ((i = stream.Read(buffer, 0, buffer.Length)) != 0)
+            foreach (TcpClient client in clients)
             {
-                message = Encoding.UTF8.GetString(buffer, 0, i);
-                Console.WriteLine("Received: " + message);
+                while ((i = clientStream[client].Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    message = Encoding.UTF8.GetString(buffer, 0, i);
+                    Console.WriteLine("Received: " + message);
 
-                //classify
-                var data = DeserializeInput(message);
+                    //classify
+                    var data = DeserializeInput(message);
 
-                Console.WriteLine(data);
+                    Console.WriteLine(data);
 
-                string response = "Server response: " + message.ToString();
-                SendMessageToClient(message: response);
+
+                }
             }
-            client.Close();
         }
     }
     catch (SocketException e)
@@ -69,61 +72,27 @@ void SetupServer()
     }
 }
 
-void OnApplicationQuit()
-{
-    stream.Close();
-    client.Close();
-    server.Stop();
-    thread.Abort();
-}
 
-void SendMessageToClient(string message)
+
+void SendMessageToClient(string message, NetworkStream stream)
 {
     byte[] msg = Encoding.UTF8.GetBytes(message);
     stream.Write(msg, 0, msg.Length);
     Console.WriteLine("Message Sent");
 }
 
-static Message DeserializeInput(string jsonString)
+void updatePlayers(TcpClient client)
 {
-    var temp = JObject.Parse(jsonString);
-    var type = temp["type"].ToString();
-
-    Message input = new Message
+    Message response = new Message
     {
-        Type = type,
-        Data = type switch
+        Type = "player",
+        Data = new PlayerData
         {
-            "input" => temp["data"].ToObject<InputData>(),
-            "player" => temp["data"].ToObject<PlayerData>(),
-            _ => null
+            Name = "Player " + playerIdByClient[client].ToString(),
+            Pos = playerPosById[playerIdByClient[client]],
+            Id = playerIdByClient[client]
         }
     };
 
-    return input;
-}
-
-public interface MessageData
-{
-    // You can define common methods or properties if needed
-}
-
-
-public class InputData : MessageData
-{
-    public List<string>? Keypresses { get; set; }
-    public List<int>? MouseClicks { get; set; }
-}
-
-public class PlayerData : MessageData
-{
-    public string? Name { get; set; }
-    public int? Hp { get; set; }
-    public int? ColorId { get; set; } //change eventually
-}
-
-public class Message
-{
-    public string? Type { get; set; }
-    public MessageData? Data { get; set; }
+    SendMessageToClient(JsonConvert.SerializeObject(response), clientStream[client]);
 }
